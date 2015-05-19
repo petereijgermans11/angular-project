4a520cfb98eea762beeae7ab0edb08a4f0a0cdac
petereijgermans11 <p.eijgermans11@gmail.com>
2015-05-19 20:29:23 +0200

    Update karma.conf.js
    jasmine unittests

--- angular demo/test/karma.conf.js
+++ angular demo/test/karma.conf.js
@@ -1,1 +1,31 @@
+module.exports = function(config){
+  config.set({
 
+    basePath : '../',
+
+    files : [
+      'web/js3rdparty/angular.js',
+	    'web/js3rdparty/angular-mocks.js',
+	    'web/js3rdparty/sockjs-min-0.3.4.js',
+      'web/js/**/*.js',
+	    'test/unittest/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
